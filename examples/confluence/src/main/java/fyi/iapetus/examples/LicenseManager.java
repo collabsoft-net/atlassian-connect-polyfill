c6ef424523bd216b5cf4d57b6aca2cc52f04d344
Remie Bolte <remie@collabsoft.net>
2024-09-10 12:37:39 +0200

    chore: update examples

---
+++ examples/confluence/src/main/java/fyi/iapetus/examples/LicenseManager.java
@@ -0,0 +1,42 @@
+
+// This file is commented out because the example plugin does not have licensing enabled
+// If you do have licensing enabled for your app, it is recommended to implement the PluginLicenseRepository.register()
+// method to ensure that the Atlassian Connect Polyfill servlet can correctly set the license state of the `lic=` parameter
+
+/*
+package fyi.iapetus.examples;
+
+import com.atlassian.plugin.Plugin;
+import com.atlassian.plugin.osgi.bridge.external.PluginRetrievalService;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.upm.api.license.PluginLicenseManager;
+import fyi.iapetus.plugins.acpolyfill.PluginLicenseRepository;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+@Component
+public class LicenseManager {
+
+    @ComponentImport
+    private final PluginLicenseRepository pluginLicenseRepository;
+    @ComponentImport
+    private final PluginRetrievalService pluginRetrievalService;
+    @ComponentImport
+    private final PluginLicenseManager pluginLicenseManager;
+
+    @Inject
+    public LicenseManager(PluginLicenseRepository pluginLicenseRepository, PluginRetrievalService pluginRetrievalService, PluginLicenseManager pluginLicenseManager) {
+        this.pluginLicenseRepository = pluginLicenseRepository;
+        this.pluginRetrievalService = pluginRetrievalService;
+        this.pluginLicenseManager = pluginLicenseManager;
+        registerLicenseManager();
+    }
+
+    void registerLicenseManager() {
+        Plugin plugin = pluginRetrievalService.getPlugin();
+        this.pluginLicenseRepository.register(plugin.getKey(), pluginLicenseManager);
+    }
+
+}
+*/
--- examples/jira/src/main/java/fyi/iapetus/examples/ACContextProvider.java
+++ examples/jira/src/main/java/fyi/iapetus/examples/ACContextProvider.java
@@ -1,6 +1,7 @@
 package fyi.iapetus.examples;
 
 import com.atlassian.plugin.PluginAccessor;
+import com.atlassian.plugin.osgi.bridge.external.PluginRetrievalService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.ApplicationProperties;
 import com.atlassian.upm.api.license.PluginLicenseManager;
@@ -8,8 +9,8 @@ import fyi.iapetus.plugins.acpolyfill.jira.AbstractACContextProvider;
 
 public class ACContextProvider extends AbstractACContextProvider {
 
-    ACContextProvider(@ComponentImport PluginAccessor pluginAccessor, @ComponentImport ApplicationProperties applicationProperties, @ComponentImport PluginLicenseManager pluginLicenseManager) {
-        super(pluginAccessor, applicationProperties, pluginLicenseManager);
+    ACContextProvider(@ComponentImport PluginLicenseManager pluginLicenseManager, @ComponentImport PluginRetrievalService pluginRetrievalService, @ComponentImport PluginAccessor pluginAccessor, @ComponentImport ApplicationProperties applicationProperties) {
+        super(pluginLicenseManager, pluginRetrievalService, pluginAccessor, applicationProperties);
     }
 
 }
